dcb0544772f6d56ed7db25ed4d3a9e69387a203b
Melissa <59567024+thulijay@users.noreply.github.com>
2020-05-29 10:13:16 +0200

    Update fruits.test.js

--- fruits.test.js
+++ fruits.test.js
@@ -18,24 +18,3 @@ describe('Counts how many apples and pears has been eaten', function(){
         assert.equal(appleAndPearEater.pearsEaten(), 2);
     });
 });
-describe('The FruitEater function ', function(){
-
-    it('should count how many apples have beed eaten', function(){
-        var appleAndPearEater = FruitEater();
-        appleAndPearEater.eatApple('apple');
-        appleAndPearEater.eatApple('apple');
-        assert.equal(appleAndPearEater.applesEaten(), 2);
-    });
-
-    it('should count how many apples and pears have beed eaten', function(){
-      
-        var appleAndPearEater = FruitEater();
-        appleAndPearEater.eatApple('apple');
-        appleAndPearEater.eatPear('pear');
-        appleAndPearEater.eatApple('apple');
-        appleAndPearEater.eatPear('pear');
-        appleAndPearEater.eatApple('apple');
-        assert.equal(appleAndPearEater.applesEaten(), 3);
-        assert.equal(appleAndPearEater.pearsEaten(), 2);
-    });
-});
